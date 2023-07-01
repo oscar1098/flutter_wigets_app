8173cc549b9d48230781cedf2391aa1f852ac1d7
oscar1098 <oscarardila1098gmail.com>
2023-06-30 22:27:50 -0500

    screens creadas y barril actualizado

---
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AnimatedScreen extends StatelessWidget {
+
+  static const name = 'Animated Screen';
+
+  const AnimatedScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class InfiniteScrollSreen extends StatelessWidget {
+
+  static const name = 'infinite_screen';
+
+  const InfiniteScrollSreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
