83cd1aa2989cf8238ee28cf6de0ca3d7751b2e10
oscar1098 <oscarardila1098gmail.com>
2023-06-30 19:22:25 -0500

    button screen finish

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MaterialApp.router(
       routerConfig: appRouter,
-      theme: AppTheme(selectedColor: 3).getTheme() ,
+      theme: AppTheme(selectedColor: 0).getTheme() ,
       debugShowCheckedModeBanner: false,
       
     );
